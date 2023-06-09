4202fa511f8417daba82287cc0484b451246d608
Admin <admin@Admins-iMac-Pro.local>
2023-06-09 16:43:40 +0530

    Tailwind css now working

---
+++ readme.md
@@ -0,0 +1,5 @@
+This is a Portfolio site of Mustafa Hakimji.
+To run this app in your local you first need to run the git command and clone thi repository.
+To clone please run this command in your terminal = `git clone https://github.com/Mustafa-Hakimji/My-Portfolio.git`
+After cloning this project please run the command `npm install`. this will install all the dependencies used in this project.
+Then run command `npm run dev` this will start this project into your local.
--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,23 @@ import {
 } from "./components";
 const App = () => {
   return (
-    <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <Navbar />
-        <Hero />
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
-      <About />
-      <Experience />
-      <Tech />
-      <Works />
-      <Feedbacks />
-      <div className="relative z-0">
-        <Contact />
-        <StarsCanvas />
-      </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
